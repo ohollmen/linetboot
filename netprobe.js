42609ff8a37f9c489fc940efddbb8f630ed3148d
Olli Hollmen <olli.hollmen@gmail.com>
2019-12-03 22:41:01 -0800

    Avoid undefined value for reverse DNS lookup.

--- netprobe.js
+++ netprobe.js
@@ -44,7 +44,9 @@ function resolve(hnode, cb) {
     if (addrs[0].address == ip_org) { prec.ipok = 1; }
     prec.addrs = addrs; // Results of resolveAny()
     addrs.forEach(function (rec) {
-      var ipaddr = rec.address; // Skip IPv6 ?
+      // Note: For 'CNAME' Recs rec.value contains name
+      // Avoid: The "name" argument must be of type string. Received type undefined
+      var ipaddr = rec.address || rec.value; // Skip IPv6 ?
       dns.reverse(ipaddr, function (err, domains) {
         if (err) { console.log("Reverse Resolution error: " + err); return cb(); }
 	if (domains[0] == hn) { prec.nameok = 1;}
