31d367bccfd151471bd466bcfb86c26fe5918146
Olli Hollmen <olli.hollmen@gmail.com>
2020-03-14 15:11:08 -0700

    Add tentative Mac probing (by ARP)

--- netprobe.js
+++ netprobe.js
@@ -1,8 +1,9 @@
 var dns = require("dns");
 var ping = require('ping');
+var arp = require('node-arp'); // pref. over arpjs (dep on libpcap)
 // var snmp = require ("net-snmp");
 var node_ssh = require('node-ssh');
-var fs = require('fs');
+var fs    = require('fs');
 var async = require("async");
 var servers;
 var resolver;
@@ -12,6 +13,7 @@ var dnsopts = {};
 var inited = 0;
 var ssh = new node_ssh();
 var pkey;
+var selfmac; // Discover own mac (for better report, where self would have mac error)
 /**
 */
 function init(opts) {
@@ -23,21 +25,26 @@ function init(opts) {
   // pkey = process.env['HOME']+'/.ssh/id_rsa';
   servers = opts.dns ? opts.dns : dns.getServers();
   //resolver.setServers([]);
+  // require('os').networkInterfaces() // [{mac: ...},{mac:...},...]
   
 }
-/** Resolve single host in DNS
+/** Resolve single host in DNS.
+* @todo Convert to async.series
 */
 function resolve(hnode, cb) {
   cb = cb || function () {};
   var hn = hnode.ansible_fqdn;
   var ip_org = hnode.ansible_default_ipv4.address;
-  var prec = {hname: hn, ip: ip_org, ipok: 0, nameok: 0}; // probe record
+  var mac_org = hnode.ansible_default_ipv4.macaddress;
+  var prec = {hname: hn, ip: ip_org, ipok: 0, nameok: 0, macok: 0}; // probe record
   var pingcfg = {timeout: 10};
   // TODO: Pull from module config
   var sshcfg = {host: hn, username: process.env['USER'], privateKey: pkey};
   var sshcmd = "uptime";
   if (typeof cb != 'function') { throw "resolve: cb is not a function !"; }
-  // resolve4, resolveAny, resolveCname
+  // dns module: resolve4, resolveAny, resolveCname
+  // Note: None of the cb() or promise calls should. The calls should *ONLY*
+  // set statistics on various probe stages (ipok, nameok, sshconn) in prec
   dns.resolveAny(hn, dnsopts, function (err, addrs) {
     if (err) { console.log("Resolution error: " + err); return cb(null, prec); }
     console.log("IPv4 Addresses: ", addrs);
@@ -50,7 +57,12 @@ function resolve(hnode, cb) {
       dns.reverse(ipaddr, function (err, domains) {
         if (err) { console.log("Reverse Resolution error: " + err); return cb(null, prec); }
 	if (domains[0] == hn) { prec.nameok = 1;}
-        console.log("Reverse: " + JSON.stringify(domains));
+        console.log("Reverse result: " + JSON.stringify(domains));
+	// Note: Lineboot host itself does not resolve. Implement self-check differentiation !
+	arp.getMAC(ipaddr, function(err, mac) {
+	  if (err) { console.log("No ARP response for "+ ipaddr); return cb(null, prec); }
+	  prec.macok = 1; // mac; // mac rdundant
+	//  
 	ping.sys.probe(ipaddr, function (isok) {
 	  prec.ping = isok;
 	  if (! isok) { return cb(null, prec); } // No use connecting, but not error
@@ -71,7 +83,9 @@ function resolve(hnode, cb) {
 	  
 	  //return cb(null, prec);
 	  
-	}, pingcfg);
+	}, pingcfg); // ping
+	  // return cb(null, prec); // NOT: ret arp Error: Callback was already called.
+	}); // arp
       });
     });
   });
@@ -100,13 +114,27 @@ function lookup(hn) {
 
 function probe_all(harr, cb) {
   var msg = "";
+  var callable = resolve;
   async.map(harr, resolve, function(err, results) {
     if (err) { return cb("Error resolving hosts: " + err); }
-    //res.json({status: "ok", data: results});
     console.log(JSON.stringify(results));
     cb(results);
   });
 }
+
+function stats_proc(hnode, cb) {
+  var cmd = "ps -ef | wc -l";
+  ssh.connect(sshcfg).then(function () {
+    ////ssh.execCommand(sshcmd, {cwd: "/"}).then(function (result) {
+    return cb(null, prec);
+    //}).catch(function () { console.log("Run error: " + hn);return cb(null, prec); });
+  }).catch(function (err) {
+    console.log("SSH Conn. Err: " + err);
+    prec.ssherr = err.toString();
+    return cb(null, prec);
+  });
+}
+
 module.exports = {
   init: init,
   probe_all: probe_all,
