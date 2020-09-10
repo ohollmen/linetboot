81b42a2f1e62163160b849f3abce2e84e80c361f
Olli Hollmen <olli.hollmen@gmail.com>
2020-09-09 17:40:51 -0700

    Refactor Redfish functionality to a library to share between ops.

---
+++ redfish.js
@@ -0,0 +1,83 @@
+/**
+ * 
+ * # Typical sequence of Setting up RFOp:
+ * 
+ * 
+ *      var rmgmt = ipmi.rmgmt_load(f, rmgmtpath);
+ *      // Instantiate by IPMI Config (shares creds)
+ *      var rfop = new redfish.RFOp("setpxe", global.ipmi);
+ *      rfop.sethdlr(hdl_redfish_succ, hdl_redfish_err);
+ *      // Call host by MC ip address
+ *      rfop.request(rmgmt.ipaddr, ipmiconf);
+ */
+var axios = require("axios");
+var ops = [
+    {"id":"boot",  "m": "post",  url: "/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset", msg: {"ResetType": "PowerCycle", } },
+    {"id":"info",  "m": "get",   url: "/redfish/v1/Systems/System.Embedded.1/"},
+    {"id":"setpxe","m": "patch", url: "/redfish/v1/Systems/System.Embedded.1/", msg: {"Boot": {"BootSourceOverrideTarget": "Pxe"}} },
+];
+var ops_idx = {}; ops.forEach(function (op) { ops_idx[op.id] = op; });
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+var isbodymeth = {"post":1, "put":1, "patch":1, };
+
+function RFOp(opid, conf) {
+  var op = ops_idx[opid];
+  this.id = op.id;
+  this.m = op.m;
+  this.url = op.url;
+  this.msg = op.msg;
+  this.conf = op.conf; // TODO: use
+}
+
+RFOp.add = function (op) {
+  if (!op.id) { throw "No ID for new Op!"; }
+  if (ops_idx[op.id]) { throw "Op by id "+op.id+" already in"; }
+  ops.push(op);
+  ops_idx[op.id] = id;
+}
+
+RFOp.prototype.makeurl = function (host, extra) {
+  if (host.match(/\/$/)) { host.replace(/\/+$/, ''); }
+  if (extra && extra.testurl) { return extra.testurl; }
+  return "https://"+host+this.url;
+};
+RFOp.prototype.sethdlr = function (succcb, errcb) {
+  this.succ = succcb;
+  this.err = errcb;
+};
+/** Make a request for the operation.
+ */
+RFOp.prototype.request = function(host, auth) {
+  var rfop = this;
+  // conf = this.conf || {};
+  if (!auth || !auth.user || !auth.pass) { throw "No Credentials"; }
+  var bauth = basicauth(auth);
+  var hdrs = { Authorization: "Basic "+bauth, "content-type": "application/json", "Accept":"*/*" }; // 
+  var msg = this.msg; // Copy ?
+  var meth = this.m; //var meth = ops[p.op];
+  if (!meth) { throw "request: meth missing in:", this; }
+  // TODO: isbodymeth[meth]
+  if (!isbodymeth[meth]) { delete(hdrs["content-type"]); msg = null; }
+  var rfurl = this.makeurl(host, auth);
+  console.log("Call("+meth+"): "+rfurl + "\nBody: ", msg, " headers: ", hdrs);
+  var reqopts = {headers: hdrs, };
+  if (!this.succ || !this.err) { throw "One of success/error handlers missing (Call sethdlr(succ,err) for this)"; }
+  if (meth == 'post') { axios[meth](rfurl, msg, reqopts).then(this.succ).catch(this.err); }
+  if (meth == 'get') { axios[meth](rfurl, reqopts).then(this.succ).catch(this.err); }
+};
+
+// https://stackabuse.com/encoding-and-decoding-base64-strings-in-node-js/
+  function basicauth(obj) {
+    // [DEP0005] DeprecationWarning: Buffer() is deprecated due to security and usability issues.
+    // Please use the Buffer.alloc(), Buffer.allocUnsafe(), or Buffer.from() methods instead.
+    var buff = new Buffer(obj.user + ":" + obj.pass); // , 'ascii'
+    return buff.toString('base64');
+  }
+  
+module.exports = {
+  RFOp: RFOp,
+  ops_idx: ops_idx,
+  basicauth: basicauth
+};
