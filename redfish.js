b1518cf5f18618197511602d1588a26377b991e5
Olli Hollmen <olli.hollmen@gmail.com>
2020-09-10 14:24:39 -0700

    Fixes and jshint tidy-ups.

--- redfish.js
+++ redfish.js
@@ -15,6 +15,8 @@ var ops = [
     {"id":"boot",  "m": "post",  url: "/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset", msg: {"ResetType": "PowerCycle", } },
     {"id":"info",  "m": "get",   url: "/redfish/v1/Systems/System.Embedded.1/"},
     {"id":"setpxe","m": "patch", url: "/redfish/v1/Systems/System.Embedded.1/", msg: {"Boot": {"BootSourceOverrideTarget": "Pxe"}} },
+    {"id":"fwinv", "m": "get",   url: "/redfish/v1/UpdateService/FirmwareInventory"}, // Also multipart POST to upload (Note ETag)
+    // "m":"post", url: UpdateService/Actions/UpdateService.SimpleUpdate} // SW URL in body
 ];
 var ops_idx = {}; ops.forEach(function (op) { ops_idx[op.id] = op; });
 
@@ -23,12 +25,12 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 var isbodymeth = {"post":1, "put":1, "patch":1, };
 
 function RFOp(opid, conf) {
-  var op = ops_idx[opid];
+  var op  = ops_idx[opid];
   this.id = op.id;
-  this.m = op.m;
+  this.m  = op.m;
   this.url = op.url;
-  this.msg = op.msg;
-  this.conf = op.conf; // TODO: use
+  this.msg = op.msg ? JSON.parse(JSON.stringify(op.msg)) : null; // Copy !
+  this.conf = op.conf; // TODO: utilize !
 }
 
 RFOp.add = function (op) {
@@ -36,7 +38,7 @@ RFOp.add = function (op) {
   if (ops_idx[op.id]) { throw "Op by id "+op.id+" already in"; }
   ops.push(op);
   ops_idx[op.id] = id;
-}
+};
 
 RFOp.prototype.makeurl = function (host, extra) {
   if (host.match(/\/$/)) { host.replace(/\/+$/, ''); }
@@ -46,6 +48,7 @@ RFOp.prototype.makeurl = function (host, extra) {
 RFOp.prototype.sethdlr = function (succcb, errcb) {
   this.succ = succcb;
   this.err = errcb;
+  return this;
 };
 /** Make a request for the operation.
  */
@@ -57,7 +60,7 @@ RFOp.prototype.request = function(host, auth) {
   var hdrs = { Authorization: "Basic "+bauth, "content-type": "application/json", "Accept":"*/*" }; // 
   var msg = this.msg; // Copy ?
   var meth = this.m; //var meth = ops[p.op];
-  if (!meth) { throw "request: meth missing in:", this; }
+  if (!meth) { throw "request: meth missing in op:" + this.id; }
   // TODO: isbodymeth[meth]
   if (!isbodymeth[meth]) { delete(hdrs["content-type"]); msg = null; }
   var rfurl = this.makeurl(host, auth);
