6171ece2570dc0f3fa607850d1bcda8a645fe6fd
Olli Hollmen <olli.hollmen@gmail.com>
2020-11-24 09:13:42 -0800

    Start Mocking up package profiles.

--- ospackages.js
+++ ospackages.js
@@ -16,17 +16,16 @@ var byhost = {}; // Pool by host
 function pkgset(oslbl, hostname, fname, opts) {
   opts = opts || {};
   if (!fs.existsSync(fname)) { throw "Package File "+fname+" not found"; }
+  var arrout = [];
+  var objout = {};
   // In JS you can do fileops w/o "file handle"
   var data = fs.readFileSync(fname, 'utf8');
   if ( ! data) { console.log("No data from package file for "+fname+" (null, undef or'')"); return null; }
   // if ( ! data.length) {}
   var arr = data.split(/\n/); // Assume single newline
-  var arrout = [];
-  var objout = {};
   var mkobj = opts.idx ? 1 : 0;
   for (var i in arr) { // i is index(!)
     var item = arr[i];
-    
     var pkgarr = item.split(/\s+/); // ["yelp", "install"]
     if (! pkgarr[0]) { continue; }
     // arr[i] = pkgarr[0];
@@ -37,6 +36,21 @@ function pkgset(oslbl, hostname, fname, opts) {
   //return {"oslbl": oslbl, "hname": hostname, "pkgs": (mkobj ? objout : arrout) };
   return (mkobj ? objout : arrout);
 }
+// borrowed from docker-imager
+function pkgs_from_txt(fname) {
+  if (!fs.existsSync(fname)) { throw "Package list file "+fname+" not found!"; }
+  let cont = fs.readFileSync(fname, 'utf8');
+  if (!cont) { throw "Package list file "+fname+" does not have content!"; }
+  var arr = cont.split(/\n/);
+  if (!arr || !arr.length) { throw "No lines found";}
+  arr = arr.filter((line) => { return (line.match(/^#/) || line.match(/^\s*$/) || !line) ? 0 : 1 ; });
+  var pkgs = arr.map((l) => { var arec = l ? l.split(/\s+/) : []; return arec[0] ? arec[0] : null; }).filter((pi) => { return pi; });
+  // pkgs.sort((a,b) => {  });
+  return pkgs;
+}
+
+/////////////////////////////// Set ops ////////////////////////////////
+
 /** Find Common (intersecting) elements of 2 arrays.
  * 
  */
@@ -75,21 +89,26 @@ function diff(a, b) {
   });
   return arr_diff;
 }
-/**
+/** Create unique union of 2 (package) sets.
+ * 
 */
 function union (a, b) {
-
+  var idx = {}; // Union
+  if ( ! Array.isArray(a)) { throw "a is not array"; }
+  if ( ! Array.isArray(b)) { throw "b is not array"; }
+  a.forEach(function (item) { idx[item] = true; });
+  b.forEach(function (item) { idx[item] = true; });
+  return Object.keys(idx); // sort((a,b) => { a.localeCompare(b); });
 }
 
 /**
 * TODO: make into an object method: pkgset.index();
 */
 function index(a) { // pkgset
+  if ( ! Array.isArray(a)) { throw "a is not array"; }
   // Array ?
   var idx = {};
-  a.forEach(function (item) {
-    idx[item] = true; // Later if (idx[item]) {}
-  });
+  a.forEach(function (item) { idx[item] = true; });
   return idx;
 }
 
@@ -98,5 +117,6 @@ function index(a) { // pkgset
 module.exports = {
   pkgset: pkgset,
   intersect: intersect,
-  diff: diff
+  diff: diff,
+  union: union
 };
