8373da3cddc94c665fde38f12a2ce9da94f5f398
Olli Hollmen <olli.hollmen@gmail.com>
2020-11-23 18:15:06 -0800

    Complete intallations for Centos/RH

--- scripts/nis_setup.sh
+++ scripts/nis_setup.sh
@@ -18,25 +18,40 @@ if [ -z "$NIS_DOM"]; then
   echo "No NIS domain is configured globally or for this machine. Skip NIS setup" >> $POST_LOG
   exit 0
 fi
-# TODO: Check OS (rh/debian)
+# Check OS (rh/debian)
+#grep Ubuntu /etc/os-release
+grep -P '(Debian GNU|Ubuntu)' /etc/os-release
+ubu_rc=$?
+# RH/Centos
+grep -P '(Red Hat|CentOS)' /etc/os-release
+cen_rc=$?
+grep 'Arch Linux' /etc/os-release
+arch_rc=$?
 # Notes: nis deps on portmap, comes with yp.conf
 # rpcbind exists for 1804, replaces portmap
-export DEBIAN_FRONTEND=noninteractive
-# Debian/Ubuntu
-sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install --no-install-recommends rpcbind nis nfs-common autofs nscd
+if [ $ubu_rc -eq 0 ]; then
+  export DEBIAN_FRONTEND=noninteractive
+  # Debian/Ubuntu
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install --no-install-recommends rpcbind nis nfs-common autofs nscd
+fi
 # RH/Centos
-#if [ ... rh/centos ... ]; then
-#  yum install yp-tools nfs-utils autofs nscd
-#fi
+# Seems yp-tools nfs-utils autofs are installed out-of-the-box ?
+if [  $cen_rc -eq 0 ]; then
+  yum install yp-tools nfs-utils autofs nscd
+fi
 
 # Set domain (todo: make backups of old)
 echo "$NIS_DOM" > /etc/defaultdomain
 echo "+$NIS_AUTO_MASTER_MAP" > /etc/auto.master
 # RH/Centos
-if [ -f "/etc/sysconfig/network" ]; then
+if [ $cen_rc -eq 0 ]; then
+  if [ ! -f "/etc/sysconfig/network" ]; then
+    touch "/etc/sysconfig/network"
+  fi
   grep '^NISDOMAIN=' /etc/sysconfig/network
   rc=$?
-  if [ $rc -ne 0 ]; then
+  # Match ...
+  if [ $rc -eq 0 ]; then
     # TODO: sed ?
     perl -pi -e 's/^NISDOMAIN=.+/NISDOMAIN={{{ nisdomain }}}/;' /etc/sysconfig/network
   else
@@ -57,6 +72,9 @@ else
   for NSERV in $NIS_SERVERS; do echo "ypserver $NSERV" >> /etc/yp.conf; done
 fi
 # Ubu 18: nis, but also ypbind works
+service ypbind enable
 service ypbind stop; service ypbind start
+service autofs enable
 service autofs stop; sleep 5; service autofs start
+service nscd enable
 service nscd stop; sleep 5; service nscd start
