94ffd9af9bc2c3f2abb8925f66886ceb132156ca
Olli Hollmen <olli.hollmen@gmail.com>
2021-04-17 13:46:33 -0700

    Fix problems under completion callback.

--- procreport.js
+++ procreport.js
@@ -7,20 +7,22 @@ var hlr = require("./hostloader.js");
 
 var tnow = 1; // 
 
-var urls = ["http://nuc5:8181/proclist","http://nuc5:8181/proclist"];
+var urls = ["http://nuc5:8181/proclist?id=1","http://nuc5:8181/proclist?id=2"];
 // axios GET calls in parallel
 //var urls = ["/u1", "/u2"];
 var reqpara = {
   // 'Content-Type': Connection: 'keep-alive', 'Access-Control-Allow-Origin': '*', withCredentials: true,
   // credentials: 'include', cookie: ...
-  headers: {},
+  headers: {}, // API Key ?
 };
 var pdata = ""; // String Or JSON
 async.map(urls, function (url, cb) {
+  //console.log("Call "+url);
   axios.get(url, reqpara).then( (resp) => {
+    //console.log("Called "+url);
     var d = resp.data;
     if (!d) { console.log("No data\n"); return cb("No Data", null); }
-    // Work with data
+    // Work with data (wrap it ?)
     
     var out = d;
     return cb(null, out);
@@ -28,31 +30,43 @@ async.map(urls, function (url, cb) {
   // 
   .catch((ex) => {
     console.log("async axios error: "+ex);
-    return cb(ex, null)});
-  },
-  function (err, results) {
+    // Sometimes UnhandledPromiseRejectionWarning: Error: Callback was already called.
+    // .. when there is a problem (implicit ex. thrown) in completion callback
+    // e.g. async axios error: ReferenceError: usermatchX is not defined
+    // where usermatchX is from func called by 
+    return cb(ex, null);
+    });
+},
+function (err, results) {
     if (err) { console.log("async.map completion error: "+ err) ; return; }
     console.log("Got results of type: "+ (typeof results)); // Says Object
-    console.log("Got results: "+ JSON.stringify(results, null, 2));
+    //console.log("Got results: "+ JSON.stringify(results, null, 2));
     //return;
     // TODO: Have analysis function as configurable callback
     var now = (Date.now() / 1000);
-    var ctx = {now: now, age: 86400*5 };
+    var ctx = {now: now, age: 86400*5, user: 'root', debug: 0};
     results.forEach((procs) => {
+      if (!Array.isArray(procs)) { console.log("One async result not an array!"); return; }
       // Pass full cmp context
       var bads = procs_analyze(procs, ctx);
+      console.log(bads.length + " procs gotten from analysis:\n", bads);
     });
   }
-);
-/** Analyze processes for stale state and return suspects
+); // async.map()
+/** Analyze processes from one host for stale state and return suspects
 */
 function procs_analyze(procs, ctx) {
   if (!Array.isArray(procs)) { console.log("No procs to analyze !"); return; }
   var bads = [];
   procs.forEach((p) => {
-    console.log(p.pid);
-    var age = ctx.now - starttime;
-    if (age > ctx.age) { console.log("Age ("+p.cmd+"): "+age); bads.push(p); }
+    ctx.debug && console.log(p.pid);
+    var age = ctx.now - p.starttime;
+    var isold = (age > ctx.age);
+    var usermatch = (ctx.user == p.owner);
+    if (isold && usermatch) { // && 
+      ctx.debug && console.log(p.pid+" owned by "+p.owner+" is old ("+p.cmd+"): "+age);
+      bads.push(p);
+    }
   });
   return(bads);
 }
