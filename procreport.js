94128c41b946e599660bbd5258cdf21dc8ad10e4
Olli Hollmen <olli.hollmen@gmail.com>
2021-04-30 16:12:18 -0700

    Wrap CLI main flow into function, create web handler.

--- procreport.js
+++ procreport.js
@@ -12,9 +12,9 @@
 var async = require("async");
 var axios = require("axios");
 var hlr   = require("./hostloader.js");
-var Getopt = require("node-getopt");
-// var tnow = 1; // 
-var seen = {};
+//var Getopt = require("node-getopt");
+var seen = {}; // Helper for testing
+var inited = 0;
 // Template candidate
 var reptmpl = `
 {{#hosts}}
@@ -22,18 +22,32 @@ var reptmpl = `
 <div id="jsGrid_badprocs" data-fsetid="" data-prochname="{{ hname }}"></div>
 {{/hosts}}
 `;
-// Process reporter config
+// Process reporter (default / base) config
 var proccfg = {nre: null, user: (process.env["PROC_USER"] || "root"), agedays: 5, allok: 0, debug: 0};
-proccfg_init(proccfg);
-// Hostloader config
-var cfg = { hostsfile: process.env["HOME"]+"/.linetboot/hosts", };
-hlr.init({ fact_path: process.env["HOME"]+"/hostinfo" });
-var hostarr = hlr.hosts_load(cfg);
-console.log("Host names:", hostarr); // DEBUG
 
-var hnames = hostarr.filter(filtercb);
-console.log(hnames.length + " hosts remain after filtering:", hnames); // proccfg.debug
-staleprocs_report(hnames, proccfg);
+function climain() {
+  proccfg_init(); // proccfg
+  // Hostloader config
+  var cfg = { hostsfile: process.env["HOME"]+"/.linetboot/hosts", };
+  hlr.init({ fact_path: process.env["HOME"]+"/hostinfo" });
+  var hostarr = hlr.hosts_load(cfg);
+  console.log("Host names:", hostarr); // DEBUG
+
+  var hnames = hostarr.filter(filtercb);
+  console.log(hnames.length + " hosts remain after filtering:", hnames); // proccfg.debug
+  staleprocs_report(hnames, proccfg, function (err, badrep) {
+    if (err) { console.log("Error in extracting process reports: "+err); process.exit(1); }
+    if (!badrep || !badrep.length) { console.log("Nothing to report (report="+badrep+")"); process.exit(1); }
+    // Decorate result set with ISO time here for CLI
+    badrep.forEach((hp) => {
+      if (!hp.procs) { return; }
+      hp.procs.forEach((p) => { p.starttime_iso = new Date(p.starttime*1000).toISOString(); });
+    });
+    console.log("Bad Processes on Hosts\n", JSON.stringify(badrep, null, 2));
+  });
+} // climain
+
+if (process.argv[1].match(/procreport.js$/)) { climain(); }
 
 // Basic / default host filtering callback.
 function filtercb (hname) {
@@ -43,17 +57,34 @@ function filtercb (hname) {
   return 0;
 }
 function filtercb2 (hpara) { if (hpara.pana) { return 1; } return 0; };
-
-function proccfg_init(proccfg) {
+// Initially passed: proccfg, but since we are tweaking module-global, leave out.
+function proccfg_init(mcfg) {
+  if (inited) { return; } // module.exports
+  console.log("procreport.init running ...");
+  if (mcfg && mcfg.procster && (typeof mcfg.procster == 'object')) {
+    console.log("procreport.init - Got mcfg");
+    // ["user","nrestr","agedays","allok"];
+    var pcfg = mcfg.procster;
+    if (pcfg.user) { proccfg.user = pcfg.user; }
+    // nrestr
+    if (pcfg.nrestr)  { proccfg.nrestr = pcfg.nrestr; }
+    if (pcfg.agedays) { proccfg.agedays = pcfg.agedays; }
+    if (pcfg.allok)   { proccfg.allok = pcfg.allok; }
+  }
+  // TODO: When not passed, use proccfg_default
+  //else { proccfg = proccfg_default; }
   // E.g. export PROC_NAMERE=^nuc or ^(bld-|[a-d][a-d]xlc)
-  proccfg.nrestr = process.env["PROC_NAMERE"];
+  if (process.env["PROC_NAMERE"]) { proccfg.nrestr = process.env["PROC_NAMERE"]; }
   proccfg.nre = proccfg.nrestr ? new RegExp(proccfg.nrestr) : null;
   if (process.env["PROC_AGEDAYS"]) { proccfg.agedays = parseInt(process.env["PROC_AGEDAYS"]); }
   if (process.env["PROC_ALLOK"] && parseInt(process.env["PROC_ALLOK"])) { proccfg.allok = 1; }
   if (process.env["PROC_DEBUG"] && parseInt(process.env["PROC_DEBUG"])) { proccfg.debug = parseInt(process.env["PROC_DEBUG"]); }
   proccfg.agesec = 86400*proccfg.agedays; // Derived, precomputed
+  inited = 1;
+  // return module.exports;
 }
-
+// Generate procster remote URL per procster conventions.
+// TODO: allow parametrizing port !
 function hosturl_formulate(host) {
   var url = "http://"+host+":8181/proclist"; // 
   seen[host] = seen[host] ? seen[host] + 1 : 1;
@@ -64,7 +95,8 @@ function hosturl_formulate(host) {
 /** Retrieve Process info asynchronously in parallel.
  * 
  */
-function staleprocs_report(hostnames, proccfg) {
+function staleprocs_report(hostnames, proccfg, cb) {
+  if (!cb) { cb = (err, badrep) => { console.log("Got bad-report: ", badrep); }; }
   // OLD: var urls = hostnames.map((hname) => { return hosturl_formulate(hname); });
   var reqpara = {
     // 'Content-Type': Connection: 'keep-alive', 'Access-Control-Allow-Origin': '*', withCredentials: true,
@@ -109,8 +141,8 @@ function staleprocs_report(hostnames, proccfg) {
   // Note: For compatibility w. eachSeries, the results should come from outer ctx
   // param rather than completion callback param.
   function (err, results) {
-      if (err) { console.log("async.map completion error: "+ err) ; return; }
-      if (!results) { console.log("No completion results"); process.exit(1);}
+      if (err) { let errmsg = "async.map completion error: "+ err; console.log(errmsg) ; return cb(errmsg, null); }
+      if (!results) { console.log("No completion results"); return cb("No compl. results", null);}
       console.timeEnd("fetch");
       console.log("Got ("+results.length+") process resultsets"); // of type: "+ (typeof results) ... Says "object" ... sigh
       // console.log("Got results: "+ JSON.stringify(results, null, 2));
@@ -119,18 +151,22 @@ function staleprocs_report(hostnames, proccfg) {
       var now = (Date.now() / 1000); // Seconds after 1970 
       var ctx = {now: now, age: 86400*proccfg.agedays, user: proccfg.user, debug: proccfg.debug}; // 5, root
       var badreport = [];
+      // TODO: Decide on error handling
       results.forEach((hp) => {
-        if (!Array.isArray(hp.procs)) { console.log("async result procs for "+hp.hname+" not an array!"); return; }
+        if (!Array.isArray(hp.procs)) { let errmsg = "async result procs for "+hp.hname+" not an array!"; console.log(errmsg); return; } // return cb(errmsg, null);
         console.log(hp.procs.length + " Processes from "+hp.hname);
-        if (!hp.procs.length) { console.log("No processes from host (possibly not reachable)"); return; }
+        if (!hp.procs.length) { let errmsg = "No processes from host (possibly not reachable)";console.log(errmsg); return; } // return cb(errmsg, null);
         // Pass full cmp context
         var bads = procs_analyze(hp, ctx);
         // TODO: Reporter callback
-        console.log(bads.length + " filtered procs gotten from analysis:\n", bads);
-        
+        ctx.debug && console.log(bads.length + " filtered procs gotten from analysis.\n");
+        (ctx.debug > 1) && console.log("Processes:\n", bads);
+        var hpb = {hname: hp.hname, procs: bads};
+        badreport.push(hpb);
       });
-      // Kill bad ...
-      
+      //console.log(badreport); // DEBUG
+      // Report (or even Kill?) bad ...
+      cb(null, badreport);
     }
   ); // async.map()
 
@@ -146,7 +182,7 @@ function procs_analyze(hp, ctx) {
   hp.bads = procs.filter((p) => {
     // https://stackoverflow.com/questions/7759237/how-do-i-pass-an-extra-parameter-to-the-callback-function-in-javascript-filter
     // var ctx = this; // TODO: Use ctx by either bind of filter 2nd param
-    ctx.debug && console.log(p.pid);
+    (ctx.debug > 1) && console.log(p.pid);
     var age   = ctx.now - p.starttime;
     var isold = (age > ctx.age);
     var usermatch = (ctx.user == p.owner);
@@ -162,3 +198,35 @@ function procs_analyze(hp, ctx) {
   //console.log("Bad ones: "+hp.bads.length);
   return(hp.bads);
 }
+/** Web based bad processreporting.
+ */
+function procreport_web(req, res) {
+  var jr = {status: "err", "msg": "Could not produce bad processes report. "};
+  proccfg.debug = 1;
+  proccfg_init(); // proccfg
+  // Assume hostloader is full init()ed, hosts loaded
+  //console.log("hlr colls: ", hlr.colls);
+  var hostarr = hlr.colls.hostnames; // Host*names*
+  console.log("Initial hosts: ", hostarr);
+  console.log("Proccfg: ", proccfg);
+  var hnames = hostarr.filter(filtercb); // Use default filter CB
+  if (!hnames || !hnames.length) { jr.msg += "No Reporting to do !"; return res.json(jr); }
+  console.log(hnames.length + " hosts remain after filtering:", hnames); // proccfg.debug
+  staleprocs_report(hnames, proccfg, function (err, badrep) {
+    if (err) { jr.msg += "Error extracting report:"+err; return res.json(jr); }
+    if (!badrep || !badrep.length) { jr.msg += "Nothing to report (report="+badrep+")"; return res.json(jr); }
+    // Decorate result set with ISO time Only on frontend !
+    //badrep.forEach((hp) => {
+    //  if (!hp.procs) { return; }
+    //  hp.procs.forEach((p) => { p.starttime_iso = new Date(p.starttime*1000).toISOString(); });
+    //});
+    //console.log("Bad Processes on Hosts\n", JSON.stringify(badrep, null, 2));
+    res.json({status: "ok", data: badrep});
+  });
+}
+
+module.exports = {
+  init: proccfg_init,
+  procreport_web: procreport_web,
+  proccfg: proccfg
+};
