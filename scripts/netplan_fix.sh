625de948f356e01ae275998595a19630fabc2850
Olli Hollmen <olli.hollmen@gmail.com>
2021-08-03 16:35:41 -0700

    Refine netplan fixing to capture first interface on multiple NIC ports. Skip non Ubuntu (18,20) OS:s.

--- scripts/netplan_fix.sh
+++ scripts/netplan_fix.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
+# Overwrite Ubuntu Netplan with network-config-wise correct values.
+# We detect the first network if here (assumed to be the one in use, but
+# note this assumption may be optimistic and fail, see $NETIF evaluation).
+# We skip any other OS:s than Ubuntu 18.04 and 20.04 that are known to use netplan.
 ## TEMPLATE_WITH: user
 # NETPLAN
 
 POST_LOG={{{ user.homedir }}}/post-log.txt
-
-NETIF=`ip addr show | grep -P -o '^\d+:\s\K(e[\w]+)'`
+# Test Netplan use
+grep -P '(\b18\.04\b|\b20\.04\b)' /etc/os-release
+usenp_rc=$?
+echo "Netplan usage: usenp_rc=$usenp_rc"
+if [ -d "/etc/netplan/" ] && [ $usenp_rc -eq 0 ]; then
+  echo "netplan_fix.sh: This OS ("`uname -a`") does not use netplan" >> $POST_LOG
+  exit 0
+fi
+# Assumes first device name is the one used for main interface.
+NETIF=`ip addr show | grep -P -o '^\d+:\s\K(e[\w]+)' | head -n 1` 
 # NETIF=`ip addr show | sed -n -e 's/^[0-9]+: \([[:alnum:]]\+\):/\1/p'
-echo "netplan_fix.sh: Detected local netif: $NETIF"
+echo "netplan_fix.sh: Detected local main netif: $NETIF" >> $POST_LOG
 
 mv /etc/netplan/01-netcfg.yaml /etc/netplan/01-netcfg.yaml.orig
-/usr/bin/curl "http://{{{ httpserver }}}/netplan.yaml?netif=$NETIF" -o /etc/netplan/01-netcfg.yaml
-echo "Downloaded new netplan: rc=$?" >> $POST_LOG
+/usr/bin/curl "http://{{{ httpserver }}}/netplan2.yaml?netif=$NETIF" -o /etc/netplan/01-netcfg.yaml
+echo "Downloaded and installed new netplan: rc=$?" >> $POST_LOG
 netplan apply
 echo "Applied netplan: rc=$?" >> $POST_LOG
 exit 0
