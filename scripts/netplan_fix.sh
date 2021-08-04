a77dd99c1dcafd4aae484871be012ebea68022bd
Olli Hollmen <olli.hollmen@gmail.com>
2021-08-04 18:11:05 -0400

    Fix Ubuntu 18.04, 20.04 match condition. make backup of original netplan by copy, not move. Use NP_PATH variable.

--- scripts/netplan_fix.sh
+++ scripts/netplan_fix.sh
@@ -7,23 +7,30 @@
 # NETPLAN
 
 POST_LOG={{{ user.homedir }}}/post-log.txt
+# NP_PATH=/target/etc/netplan
+NP_PATH=/etc/netplan
 # Test Netplan use
 grep -P '(\b18\.04\b|\b20\.04\b)' /etc/os-release
 usenp_rc=$?
 echo "Netplan usage: usenp_rc=$usenp_rc" >> $POST_LOG
 # Note /etc/netplan might be /target/etc/netplan at this point
-# [ -d "/etc/netplan/" ] && ...
-if [ $usenp_rc -eq 0 ]; then
+# [ -d "$NP_PATH" ] &&
+if [ $usenp_rc -ne 0 ]; then
   echo "netplan_fix.sh: This OS ("`uname -a`") does not use netplan" >> $POST_LOG
+  cat /etc/os-release >> $POST_LOG
   exit 0
 fi
 # Assumes first device name is the one used for main interface.
 NETIF=`ip addr show | grep -P -o '^\d+:\s\K(e[\w]+)' | head -n 1` 
 # NETIF=`ip addr show | sed -n -e 's/^[0-9]+: \([[:alnum:]]\+\):/\1/p'
 echo "netplan_fix.sh: Detected local main netif: $NETIF" >> $POST_LOG
-
-mv /etc/netplan/01-netcfg.yaml /etc/netplan/01-netcfg.yaml.orig
-/usr/bin/curl "http://{{{ httpserver }}}/netplan2.yaml?netif=$NETIF" -o /etc/netplan/01-netcfg.yaml
+# DEBUG
+ls -al /etc/netplan/* >> $POST_LOG
+ls -al /target/etc/netplan/* >> $POST_LOG
+# Changed move to cp
+cp -p $NP_PATH/01-netcfg.yaml $NP_PATH/01-netcfg.yaml.orig
+# Curl *will* overwrite w/o complaints
+/usr/bin/curl "http://{{{ httpserver }}}/netplan2.yaml?netif=$NETIF" -o $NP_PATH/01-netcfg.yaml
 echo "Downloaded and installed new netplan: rc=$?" >> $POST_LOG
 netplan apply
 echo "Applied netplan: rc=$?" >> $POST_LOG
