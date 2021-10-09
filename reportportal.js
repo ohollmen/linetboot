0c767b81ad06dcefa4840c3b1a2c56934e05a141
Olli Hollmen <olli.hollmen@gmail.com>
2021-10-08 18:12:39 -0700

    Migrate RP member addition towards web API.

--- reportportal.js
+++ reportportal.js
@@ -14,30 +14,37 @@ var axopt = { headers: {Authorization: "bearer "+cfg.atok}};
 var burl = cfg.url; // API URL
 var memrole = cfg.memrole; // "MEMBER";
 var usernames = [];
-
-// Because LDAP users cannot be explicitly created (only impl. by logging in)
-// Load the users from RP (user/all)
-// loadusers_rp((err, users) => { console.log("Loaded users!"); });
-// var cb = curl_cmds_gen; proj_load(cb);
-
-//proj_load(curl_cmds_gen);
-async.parallel([ loadusers_rp, proj_load ], (err, ress) => {
-  if (err) { console.log("Failed either users load or projects load:"+ err); process.exit(1); }
-  console.error("Got users ",ress[0],"and projects "+ress[1]+"");
-  var users = ress[0];
-  var projs = ress[1];
-  usernames = users.map((u) => { return u.userId; }); // AoO => AoS
-  curl_cmds_gen (null, projs, usernames);
-});
-//async.waterfall([loadusers_rp, ], (err, ress) => { console.log("WF-COMPLETE"); });
-
+var debug = 0;
+if (process.argv[1].match("reportportal.js")) {
+  if (process.env["RP_DEBUG"]) { debug = 1; }
+  createusers((err) => {
+    
+  });
+}
+/** Add all logged-in network users to RP groups.
+ * Do this because LDAP users cannot be explicitly created (only impl. by logging in)
+ * - Load the users and projects from RP (user/all)
+ * - Detect which groups need member addition
+ * E.g. loadusers_rp((err, users) => { console.log("Loaded users!"); }); proj_load(cb);
+ */
+function createusers(cb) {
+  async.parallel([ loadusers_rp, proj_load ], (err, ress) => {
+    if (err) { console.log("Failed either users load or projects load:"+ err); process.exit(1); }
+    debug && console.error("Got users ",ress[0],"and projects "+ress[1]+"");
+    var users = ress[0];
+    var projs = ress[1];
+    usernames = users.map((u) => { return u.userId; }); // AoO => AoS
+    curl_cmds_gen (null, projs, usernames);
+  });
+  //async.waterfall([loadusers_rp, ], (err, ress) => { console.log("WF-COMPLETE"); });
+}
 /** Load RP Users. Narrow userlist down to LDAP users only. */
 function loadusers_rp(cb) {
   axios.get(cfg.url+"user/all", axopt).then((resp) => {
     var d = resp.data;
     var users = d.content;
     users = users.filter((u) => { return u.accountType == 'LDAP'; });
-    //console.log("LDAP-Users:", users);
+    //debug && console.log("LDAP-Users:", users);
     return cb(null, users);
   }).catch((ex) => {
     console.error("Failed loading RP users:"+ex);
@@ -67,13 +74,13 @@ function curl_cmds_gen (err, projs, usernames) {
 function proj_load(cb) {
   axios.get(cfg.url+"project/list", axopt).then((resp) => {
     var d = resp.data;
-    // console.log(d);
+    // debug && console.log(d);
     var arr = d.content.filter((p) => { return p.entryType == 'INTERNAL'; });
     // console.log(arr); // process.exit(1);
-    console.error("Fetch "+arr.length+" projects");
+    debug && console.error("Fetch details on "+arr.length+" projects");
     async.map(arr, projget, function (err, ress) {
-      if (err) { console.error("Proj. details Error: "+err);  return cb(err, null);  } // // process.exit(1);
-      console.error("PROJECTS:"+ JSON.stringify(ress, null, 2));
+      if (err) { console.error("Proj. details Error: "+err);  return cb(err, null);  }
+      debug && console.error("PROJECTS:"+ JSON.stringify(ress, null, 2));
       // Call cb with ress.
       return cb(null, ress);
     });
@@ -88,7 +95,7 @@ function proj_load(cb) {
 // Call the callback with details data.
 function projget(p, cb) {
   var pn = p.projectName;
-  console.error("fetching project: "+pn);
+  debug && console.error("fetching individual project: "+pn);
   axios.get(cfg.url+"project/"+pn, axopt).then((resp) => {
     // OK
     return cb(null, resp.data);
@@ -96,11 +103,13 @@ function projget(p, cb) {
     return cb(ex, null);
   });
 }
-// Check (syncronously) all groups and add member(s) as needed.
-// Return member addition structure for memberships that need to be added.
+/* Check (syncronously) all groups and add member(s) as needed.
+ * Return member addition structure for memberships that need to be added.
+ * Data is indexed by project (use in url) and the values are ready-to-use
+ * as POST data in member addition.
+ */
 function projects_add_mem(projs, usernames) {
-  console.error("Projs(to-add-mems-to): "+projs);
-  //console.log("END !"); process.exit(1);
+  debug && console.error("Projs(to-add-mems-to): "+projs);
   // Projects
   var allpadds = {};
   try {
@@ -110,7 +119,7 @@ function projects_add_mem(projs, usernames) {
     usernames.forEach(function (user) {
       // Check user presence
       var u = p.users.find((u) => { return u.login == user; });
-      if (u) { console.error(user+" already in project "+p.projectName+ " (in role:"+u.projectRole+")"); return; }
+      if (u) { debug && console.error(user+" already in project "+p.projectName+ " (in role:"+u.projectRole+")"); return; }
       // Else add ...
       padd[user] = memrole;
       allpadds[pn] = padd;  
@@ -119,7 +128,7 @@ function projects_add_mem(projs, usernames) {
   } catch (ex) {
     console.error("projects_add_mem: Error checking/adding members: "+ex);
   }
-  console.error(allpadds);
+  debug && console.error(allpadds);
   return allpadds;
 }
 // TEST: Creating users as LDAP users will NOT work.
