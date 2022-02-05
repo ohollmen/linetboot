adc5ff2ca5cfb00923013570ca3ac380eae44361
Olli Hollmen <olli.hollmen@gmail.com>
2022-02-05 14:06:30 -0800

    Pick right data structure branch intelligently.

--- test/esxi_host_prep.js
+++ test/esxi_host_prep.js
@@ -94,7 +94,7 @@ function json_merge() {
     var cont = fs.readFileSync(fname, 'utf8');
     j = JSON.parse(cont);
   }
-  catch(ex) { usage("Error parsing JSON  ("+fname+"): "+ex); };
+  catch(ex) { usage("Error parsing JSON  ("+fname+"): "+ex); }
   if (!j) { usage("Error parsing JSON ("+fname+"), did hot get DS-handle"); }
   //var fs = require("fs");
   //var stdinBuffer = fs.readFileSync(0); // STDIN_FILENO = 0
@@ -108,11 +108,9 @@ function json_merge() {
     var jin = JSON.parse(sidata);
     if (!jin) { usage("No Proper JSON from STDIN!"); }
     console.error(JSON.stringify(jin, null, 2));
-    Object.keys(jin).forEach((k) => {
-      if (j[k]) { usage("Key "+k+" already in target JSON."); }
-      console.log("Merging "+k);
-      j[k] = jin[k];
-    });
+    
+    // Merge
+    merge(jin, j);
     console.log(JSON.stringify(j, null, 2));
   });
   if (process.argv.includes("--save")) {
@@ -120,12 +118,19 @@ function json_merge() {
     fs.writeFileSync( fname, JSON.stringify(j, null, 2), {encoding: "utf8"} );
   }
 
-  // Patch / merge
-  
-  //console.log(JSON.stringify(j, null, 2));
-  function processLine (line) {
-    console.log(line + '!')
+  // Patch / merge. Internally use branch j.ansible_facts
+  function merge(jin, j) {
+    if (j.ansible_facts) { j = j.ansible_facts; }
+    Object.keys(jin).forEach((k) => {
+      if (j[k]) { usage("Key "+k+" already in target JSON."); }
+      console.log("Merging "+k);
+      j[k] = jin[k];
+    });
   }
+  //console.log(JSON.stringify(j, null, 2));
+  //function processLine (line) {
+  //  console.log(line + '!')
+  //}
 }
 var ops = {netinfo: make_netinfo, merge: json_merge};
 var argv2 = process.argv.slice(2);
