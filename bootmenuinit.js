1ae16c9ffdfb828f8503c4db0fda64a7cbb537d2
Olli Hollmen <olli.hollmen@gmail.com>
2025-04-23 10:27:52 -0700

    Modernize the utility (in anticipation for changes).

--- bootmenuinit.js
+++ bootmenuinit.js
@@ -9,25 +9,37 @@ var Mustache = require("mustache");
 var path = require('path');
 var fs = require('fs');
 var cfg = require("./bootables.json");
-if (!cfg) { error_exit("No bootables.json loaded\n"); }
-if (!cfg.items) { error_exit("No bootable items (cfg.items)\n"); }
-var op = process.argv[2];
 
 var ops = {
-  "menu": genmenu, // {desc: "Generate Simple menu stub (w/o advanced append options)", cb: genmenu},
-  "mount": mount, // {desc: "Generate loop-mount commands for ISO media)", cb: mount},
-  "fstab" : mount, // 
-  "download": download, // {desc: "Generate ISO media download commands)", cb: download},
-  "validate_ki": validate_ki, // {"desc":"Validate Mounted media kernel and initrd file presence", cb: validate}
+  "menu": {desc: "Generate Simple menu stub (w/o advanced append options)", cb: genmenu},
+  "mount": {desc: "Generate loop-mount commands for ISO media)", cb: mount},
+  "fstab" : {desc: "Generate fstab", cb: mount},
+  "download": {desc: "Generate ISO media download commands)", cb: download},
+  "validate_ki": {"desc":"Validate Mounted media kernel and initrd file presence", cb: validate_ki}
 };
-cfg.items.forEach(function (it) {
-  if (it.img_bn) { return; } // Already given explicitly as likely not extractable from URL
-  it.img_bn = path.basename(it.url); // "foo-1.2";
-});
-if (!ops[op]) { error_exit("No op:" + op); }
+function usage(msg) {
+  var rc = 1;
+  console.error("menuinit Error: " + msg);
+  // Brief usage
+  console.log("Available ops/subcommands:\n"+ Object.keys(ops).map( (k) => { return `- ${k} - ${ops[k].desc}`; }).join('\n'));
+  process.exit(rc);
+}
+if (process.argv[1].match("bootmenuinit.js")) {
+  // TODO: take httpserver, mountpath, imgpath from main config (unless overriden)!
+  let mcfgfn = `${process.env.HOME}/.linetboot/global.conf.json`;
+  let mcfg = require(mcfgfn);
+  if (!cfg) { usage("No bootables.json loaded\n"); }
+  if (!cfg.items) { usage("No bootable items (cfg.items)\n"); }
+  cfg.items.forEach(function (it) {
+    if (it.img_bn) { return; } // Already given explicitly as likely not extractable from URL
+    it.img_bn = path.basename(it.url); // "foo-1.2";
+  });
+  var op = process.argv[2];
+  if (!ops[op]) { usage("No op:" + op); }
 
-ops[op]();
-process.exit(0);
+  ops[op].cb();
+  process.exit(0);
+}
 // Validate kernel / boot items
 function validate_ki () {
   var attrs = ["kernel","initrd"];
@@ -49,13 +61,7 @@ function validate_ki () {
   }
 }
 
-function error_exit(msg) {
-  var rc = 1;
-  console.error("menuinit Error: " + msg);
-  // Brief usage
-  console.log("Available ops: "+ Object.keys(ops).join(', ') + ".");
-  process.exit(rc);
-}
+
 function genmenu() {
   // Patch with config "httpserver"
   cfg.items.forEach(function (it) {
