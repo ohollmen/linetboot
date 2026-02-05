3c1fed3f0e303fae26a231b0bd522a7fe948d453
Olli Hollmen <olli.hollmen@gmail.com>
2026-02-05 09:58:43 -0800

    Allow adding op/subcommand to CLI callback opts. Add clarifying comments and doc.

--- cliapp.js
+++ cliapp.js
@@ -1,8 +1,10 @@
-/** Utility library to create SLI apps "the easy way".
+/** Utility library to create CLI apps "the easy way".
 * Handles extracting subcommands and parses command line (Using GetOpt).
 * Holds the structural state of CLI app w.
-* - subcommands
+* - subcommands - In format: Array of Objects with id,label,cb (Note: names of members id,label are configurable)
 * - options (in GetOpt format)
+* 
+* One can customize naming of members by setting (e.g.): cliapp.attrs.id = 'ID'; cliapp.attrs.label = 'title';
 * ## TODO
 * - Add a serializer to inline back a hard-wired pattern of ops (as source code)
 * - Add a serializer for ther languages / runtimes (for same ops, optdesc)
@@ -29,7 +31,10 @@ function cliapp(ops, optdesc, opts) {
   this.attrs = {id: "id", label: "label"};
 }
 /** Extract subcommand and Parse CLI.
-* Operate on global process object: process.argv.
+* Operate on global process object: process.argv. This function avoids modifying by copying
+* process.argv for its purposes (TODO: Check slice() behavior !!!).
+* Params in para:
+* - addself - Adds cliapp self-instance to opts (so that self will be passed to handler if opts is passed)
 */
 cliapp.prototype.parse = function(para) {
   var argv2 = process.argv.slice(2);
@@ -41,10 +46,11 @@ cliapp.prototype.parse = function(para) {
   let getopt = new Getopt(this.optdesc);
   let opt = getopt.parse(argv2);
   let opts = opt.options;
-  // Delete short options as redundant
+  // Delete short options as redundant (for cleanness)
   for (k in opts) { if (k.length == 1) { delete opts[k]; }}
   if (!opts) { opts = {}; } // Ensure an object - even if empty
   this.opts = opts;
+  if (para.addop) { opts.op = op; }
   if (para.addself) { opts.self = dclone(this); } // dclone() creates a non-object-instance
   // self-as-(module)-global ???
   // For flexibility - return the node / description for the current subcommand / operation
@@ -58,8 +64,10 @@ cliapp.prototype.dump = function () {
 cliapp.prototype.usage = function (msg) {
   if (msg) { console.error(msg); }
   console.error("Use one of the subcommands:");
+  if (this.debug > 5) { console.error(this);   } // process.exit();
+  //console.error(this); // Force-DEBUG
   // Arrow-f allows this to be this in deeper scope
-  console.error( this.ops.map( (opn) => { return `${opn[this.attrs.id]} - ${opn[this.attrs.id]}`; }).join("\n") );
+  console.error( this.ops.map( (opn) => { return `- ${opn[this.attrs.id]} - ${opn[this.attrs.label]}`; }).join("\n") );
   console.error("CLI Options supported:");
   console.error( this.optdesc.map( (o) => { return `- ${o[1]} - ${o[2]}`; }).join("\n") );
   process.exit(1);
@@ -85,6 +93,6 @@ if (process.argv[1].match("cliapp.js")) {
   // - Initialize app: init(cfg)
   let app = new cliapp.cliapp(ops, cliopts, {});
   let opn = app.parse({ addself: true });
-  let rc  = opn.cb(app.opts);  
+  let rc  = opn.cb(app.opts);
   
 }
