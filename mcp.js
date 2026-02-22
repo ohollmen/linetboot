9206a42121601cb24cedc924d9fd7fee9d9cb44a
Olli Hollmen <olli.hollmen@gmail.com>
2026-02-21 22:15:05 -0800

    Work on streaming (SSE) implementation (AI clinet interop seems pretty bad). Plan to alter stdio implementation from line based request and responses to having http (style) headers.

--- mcp.js
+++ mcp.js
@@ -21,7 +21,8 @@
  *   - Most AI have config dir in ~ and `skills/` dir under it (E.g. .claude, .copilot, .gemini, .codex - for chatGPT)
  * - .outputSchema: https://github.com/modelcontextprotocol/modelcontextprotocol/pull/371
  * - Superficial article on schemas (w. examples): https://www.merge.dev/blog/mcp-tool-schema
- * 
+ * - https://github.com/modelcontextprotocol/typescript-sdk
+ * -https://modelcontextprotocol.wiki/en/docs/concepts/resources
  * ## Client side configuration
  * 
  * - https://platform.claude.com/docs/en/agent-sdk/mcp
@@ -34,13 +35,17 @@ let cproc     = require("child_process");
 let readline  = require('readline'); // for stdio transport. In node.js core !
 // let jrpc  = require('jrpc');
 // Standard MCP "capabilities" (from initialize - capability negotiation)
-let stdcapa = { "capabilities": {
+let stdcapa = { "protocolVersion": "2025-11-25", "capabilities": {
     "tools": {}, // Also: "resources": {}, "prompts": {},
-}};
+  },
+  "serverInfo": { "name": "my-mcp-server", "version": "1.0.0" }
+};
 
 // TODO: Require more props, e.g. if handler should be run in async or sync mode (!)
-// Mandatory: initialize Practically mandatory tools/list
+// Mandatory: initialize Practically mandatory tools/list.
+// See if rpc.discover is fit here (?): return res.json({ jsonrpc: "2.0", result: schema, id: id, });
 let method_hdlrs = {
+  // MCP JSON-RPC level handlers (NOT *tools* level - tools/call will call tool handler)
   "initialize": (method, id, params) => { return { result: stdcapa }; },
   // loop through
   "tools/list": (method, id, params) => { return { jsonrpc: "2.0", result: {tools: [schema_tool] }, id: id, }; },
@@ -48,6 +53,8 @@ let method_hdlrs = {
     let tname = params.name;
     return tools_hdlrs[tname](tname, id, params.arguments); // ret. result
   },
+  // This is *notification* - No body response
+  "notifications/initialized":  (method, id, params) => { return { jsonrpc: "2.0", result: {}, id: id, }; },
   "uptime": hdl_uptime,
 };
 // These should have similar/same signature (method, id, params) as method calls
@@ -56,7 +63,7 @@ let tools_hdlrs = {
   "uptime": hdl_uptime,
 };
 let jrpc = {}; // JRPC module / NS "method_hdlr": {} ?
-
+// Add JSON-RPC Handler to module-global (for now) dispatch table.
 function method_hdlr_add(mname, mhdlr) {
   if (typeof mhdlr != 'function') { console.error(`Method handler passed is not a function !`); return; }
   method_hdlrs[mname] = mhdlr;
@@ -70,8 +77,10 @@ function safeExec(cmd) {
     throw new Error(`Shell command failed: ${err.message}`);
   }
 }
-// Create a wrapping for a tool call for client (there is a 2nd stage indirection here). Let caller add id (?)
-function mcp_tools_call(tname, params) {
+/** Create a wrapping for a tool call for client (there is a 2nd stage indirection here). Let caller add id (?)
+ * @return request message (to be sent out by client).
+*/
+function mcp_tools_call_msg(tname, params) {
   let m = { // "jsonrpc": "2.0",
     method: "tools/call", params: { name: tname, arguments: params } };
   return m;
@@ -105,7 +114,7 @@ function jsonRpcError(id, code, message) {
   return { jsonrpc: "2.0", error: { code: code, message: message }, id: id || null, };
 }
 
-/** Service Schema (for MCP discovery)
+/** Service Schema (for MCP discovery rpc.discover - see how official the standard it)
  * TODO: Isolate these to JS "plugin" modules.
  * Allow module to export one or array of these to then register here.
  * Note: It seems intial documentation on schema format was wrong/misleading - revise this.
@@ -166,7 +175,7 @@ function hdl_uptime(method, id, params) {
   const allowed = ["startiso", "minutes", "hours", "days", "raw"];
   if (!allowed.includes(restype)) {
     //return res.json(
-    return jsonRpcError(id, -32602, `Invalid 'restype'. Allowed: ${allowed.join(", ")}`)
+    return jsonRpcError(id, -32602, `Invalid 'restype' (${restype}). Allowed: ${allowed.join(", ")}`)
     //);
   }
 
@@ -196,14 +205,87 @@ function hdl_uptime(method, id, params) {
   }
   return { result: result, };
 }
+
 // Thin wrapper for HTTP / Express.js
-function hdl_rpc_req_http(req, res) {
-  let m = req.body;
+function hdl_rpc_req_express(req, res) {
+  let m = req.body; // Auto-parsed by express
   //const { jsonrpc, method, params, id } = req.body;
-  hdl_rpc_req(m, res);
+  let rp = hdl_rpc_req(m, res);
+  //if (rp) res.json();
 }
-/** JSON-RPC level handler (for any of the transport types: http, stdio) */
-//function hdl_rpc_req(req, res) {
+
+function jrpc_is_notif(m) {
+  return (!("id" in m) && !("params" in m)) || false;
+}
+
+/** Handle request by node.js "http" (raw server module, NO express) in SSE manner
+ * E.g. codex defaults to HTTP/SSE in MCP interaction (No way to use/configure "traditional" stateless HTTP).
+ * See: Server-Sent Events (SSE),  EventSource API in browsers
+ * codex sends in header: "accept":"text/event-stream, application/json"
+ * when url is present instead of command (in config), Codex treats it as HTTP/SSE transport automatically.
+ */
+function hdl_rpc_req_http_raw(req, res) {
+  if (req.method != "POST") { res.statusCode = 404; return res.end(); }
+  console.error(`Got raw ${req.method} req (on URL: '${req.url}')`);
+  let streamhdr = { 'Content-Type': 'application/json',
+    'Transfer-Encoding': 'chunked', 'Connection': 'keep-alive' }; // Need to change Content-Type to text/event-stream ?
+  //res.statusCode = 200; // Express equivalent: res.status(200);
+  let streamhdr2 = { 'Content-Type': 'text/event-stream', 'Cache-Control': 'no-cache', 'Connection': 'keep-alive' };
+  res.writeHead(200, streamhdr2); // Also: res.setHeader(k, v);
+  res.write("\n"); // Required initial flush
+  let buffer = ''; // Cumulated JSON-RPC message buffer to parse as JSON.
+  // (Monkey) patch res to be compatible with express res.json(d)
+  // This will be unecessary soon with move to handle output here (res.write(....))
+  res.json = (d) => {
+    console.error(`.json() sending out: ${JSON.stringify(d)}`);
+    res.write("data: "+ JSON.stringify(d) + "\n\n"); // Should we have "\n\n" for SSE ?
+    //res.end() // ? w/o this e.g. curl (used for testing) 1) does not close conn, takes long to output response (after timeout ?)
+  }; 
+  req.on('data', (chunk) => {
+    console.error(`Got data chunk: ${chunk.length} B`);
+    buffer += chunk.toString();
+  });
+  /*
+    try {
+      const m = JSON.parse(buffer); // TODO: Move to req.on('end', ...) handler
+      console.error(`Parsed buffer (${buffer.length} B): ${JSON.stringify(m)}`);
+      //buffer = ''; // reset if NO exceptions
+      //handleMessage(res, msg);
+      try {
+        let rp = hdl_rpc_req(m, res); // For now rp unused
+        //TODO: if (rp) res.write(JSON.stringify(rp));
+        console.log(`res-finished: ${res.finished}`); // This seems to correspond to res.end() being called.
+      } catch(ex) { console.error(`RPC Handling error: ${ex}`); }
+    } catch (ex) {
+     console.error(`More data needed:${ex} (Got C: ${chunk.length} B: ${buffer.length}, HSENT: ${res.headersSent}`);
+     // wait for more data
+    }
+  //});
+  */
+  // Finished receiving request body from client //  ${req.headers['user-agent']}
+  // Socket should stay open despite this. TODO: We should parse (collected buffer) and handle jrpc here !!
+  req.on('end', () => {
+    console.error(`req-end - Client has sent all of request BODY (${buffer.length})`); // (Buf: ${buffer.length} B)
+    if (!buffer.length) { }
+    const m = JSON.parse(buffer);
+    console.error(`Parsed buffer (${buffer.length} B): ${JSON.stringify(m)}`);
+    let rp = hdl_rpc_req(m, res);
+  });
+  res.on('close', () => {console.error(`res-close - Response stream closed (by server)`); })
+  // Added for *connection* close detection.
+  req.socket.on('close', () => {
+    // by 
+    console.error(`TCP socket closed (${JSON.stringify(req.headers)})`); // ${JSON.stringify(req.headers)}
+    console.error(`  - Requested by ${req.headers['user-agent']}`);
+  });
+}
+
+/** JSON-RPC level handler (for any of the transport types: http, stdio)
+ * 
+ * @param m - Incoming JSON-RPC Message
+ * @param res - "http" or express response (we have monkey-patched http resp so it can handle .json() )
+ * TODO: Return message only to not have to do with output methods of any of the specific transports and libraries (http, express).
+ */
 function hdl_rpc_req(m, res) {
   //let m = null; // Message
   //const { jsonrpc, method, params, id } = m = req.body;
@@ -213,28 +295,35 @@ function hdl_rpc_req(m, res) {
   
   if (jsonrpc !== "2.0") { return res.json(jsonRpcError(id, -32600, "Invalid JSON-RPC version")); }
   if (!method)           { return res.json(jsonRpcError(id, -32600, "Missing method")); }
+
+  console.error(`FULL-REQ: ${JSON.stringify(m, null, 2)}`);
+  // Notification. in future: return null; res.end() here causes: Error [ERR_STREAM_WRITE_AFTER_END]: write after end
+  // res.write("");  res.end(); # E(only) closes sockets (at timeout ? Transport channel closed, when send initialized notification)
+  // W+E Same
+  // W - Client does not close immed, but doesnot recv (empty) resp either (waits for 100s),res-finished: false (due to no res.end())
+  // res.end(); res.statusCode = 200; <= too late here. DO not even do this on SSE: res.write("data: \n\n");
+  // For notifications: You send nothing. Just accept them. Do NOT close connection.
+  //if (typeof id != 'number') { console.error("NOTIF: Send empty 200");   return; }
   try {
-    // SCHEMA DISCOVERY (JSON-RPC)
-    /*
-    if (method === "tools/list") { // rpc.discover
-      //return res.json({ jsonrpc: "2.0", result: schema, id: id, });
-      return res.json({ jsonrpc: "2.0", result: {tools: [schema_tool] }, id: id, });
-    }
-    */
+    console.error(`Got RPC Request: id: ${id}, method: ${method}), params: ${JSON.stringify(params)}`);
+    // if (!("id" in m)) { return res.send(); } // Notification
     let result = null; // Rename: jres or jrpcres
     // Dispatch method from method_hdlrs (TODO: make more granular)
     let mcb = method_hdlrs[method];
-    if (mcb) {
-      result = mcb(method, id, params); // rmsg ?
-      //if (typeof result != 'object') { return res.json(jsonRpcError(id, -32603, "No result object returned from RPC handler !!!")); }
-      if (result.error) { return res.json(result); } // Assume complete response (?) OR add result.id = id; result.jsonrpc = "2.0";
-      // Still check result.result (for saneness) for value and object (array, even string ?) type ?
-      // At JSON-RPC Handling level: Now create complete response with "jsonrpc" and "id"
-      result.jsonrpc = "2.0";
-      result.id = id;
-      return res.json(result); // res.json({ jsonrpc: "2.0", result: result, id: id, });
-    }
-    else { return res.json(jsonRpcError(id, -32601, "Method not found")); }
+    // Detect notification, do not respond
+    if (jrpc_is_notif(m)) { console.error("NOTIF: Send empty 200");   return; }
+    if (!mcb) { return res.json(jsonRpcError(id, -32601, `Method '${method}' not found`)); }
+    console.error(`Found handler for m: '${method}'`);
+    result = mcb(method, id, params); // rmsg ?
+    if (typeof result != 'object') { return res.json(jsonRpcError(id, -32603, "No result object returned from RPC handler !!!")); }
+    if (result.error) { return res.json(result); } // Assume complete response (?) OR add result.id = id; result.jsonrpc = "2.0";
+    // Still check result.result (for saneness) for value and object (array, even string ?) type ?
+    // At JSON-RPC Handling level: Now create complete response with "jsonrpc" and "id"
+    result.jsonrpc = "2.0";
+    result.id = id;
+    return res.json(result); // res.json({ jsonrpc: "2.0", result: result, id: id, });
+    // }
+    // else { 
   } catch (err) {
     //let msg = `Error during json-rpc handling: ${err.message}`;
     return res.json(jsonRpcError(id, -32603, err.message));
@@ -242,18 +331,27 @@ function hdl_rpc_req(m, res) {
 }
 module.exports = {
   hdl_rpc_req: hdl_rpc_req,
-  hdl_rpc_req_http: hdl_rpc_req_http, // For arbitrary express applications.
+  hdl_rpc_req_express: hdl_rpc_req_express, // For arbitrary express applications.
 };
 if (process.argv[1].match("mcp.js")) {
   let PORT = process.env.PORT || 3000;
-  let mode = process.argv[2] || "http"; // http / stdio
-  let servurl = "/rpc";
-  console.log(`Got mode: ${mode}`);
-  // HTTP transport
+  let mode = process.argv[2] || "http"; // http / httpraw / stdio
+  let servurl = "/mcp"; // "/rpc" works equally
+  console.error(`Got mode: ${mode}`);
+  let res = null;
+  // HTTP transport - HTTP/SSE transport
+  if (mode == 'httpraw') {
+    // Raw Node.js
+    let http = require("http");
+    let server = http.createServer(hdl_rpc_req_http_raw);
+    server.listen(PORT, () => {
+      console.log('Streamable HTTP MCP server running on port 3000');
+    });
+  }
   if (mode == 'http') {
     let app = express();
     app.use(express.json());
-    app.post(servurl, hdl_rpc_req_http);
+    app.post(servurl, hdl_rpc_req_express);
     app.listen(PORT, () => {
       console.log(`MCP (HTTP) JSON-RPC server running on port ${PORT} (Service URL path: '${servurl}')`);
     });
@@ -261,13 +359,28 @@ if (process.argv[1].match("mcp.js")) {
   // stdio transport
   else if (mode == 'stdio') {
     // Create (i.e. "mock') an object that looks similar to Express res, w. method json() to share the response handling interface 1:1
-    let res = {json: (rmsg) => {
-      process.stdout.write(JSON.stringify(rmsg) + '\n');
-    }};
+    //let
+    res = {json: (rmsg) => {
+        let j = JSON.stringify(rmsg);
+        process.stdout.write(`Content-length: ${j.length}\n\n`);
+        process.stdout.write( j ); // + '\n'
+      },
+      send: (rmsg) => { process.stdout.write(rmsg + '\n'); },
+      write: (rmsg) => { process.stdout.write(rmsg + '\n'); },
+      status: (any) => { return res; }, // for chaining - should not be used
+    };
     // output => Don't let readline touch stdout, terminal => stdin is not a TTY.
     const rl = readline.createInterface({ input: process.stdin, output: null, terminal: false });
+    console.error(`Hook handlers`);
     // Request handler. Note *ALL* JSON of request must be on one line.
-    rl.on('line', (line) => {
+    rl.on('line', hdl_rpc_req_stdio_msg);
+    // Register "client close" handler for stdio mode (stdin closed - client disconnected, clean up and exit)
+    // There is an explicit: rl.close()
+    rl.on('close', () => { process.exit(0); }); // Do fancier stuff here ?
+
+  }
+   function hdl_rpc_req_stdio_msg(line) { // TODO: hdl_rpc_req_stdio_msg(res)(line)
+      console.error(`Got line: ${line}`);
       let trimmed = line.trim();
       if (!trimmed) { return; } // skip empty lines (i.e. requests)
       let message;
@@ -280,13 +393,9 @@ if (process.argv[1].match("mcp.js")) {
         return;
       }
       // Parsed as JSON ok - process JSON-RPC.
-      hdl_rpc_req(message, res);
+      let rp = hdl_rpc_req(message, res);
+      
       // OLD/TEST - Write response as a single line to stdout. Now written with hdl_rpc_req using res.json()
-      // process.stdout.write(JSON.stringify(response) + '\n');
-    });
-    // Register "client close" handler for stdio mode (stdin closed - client disconnected, clean up and exit)
-    // There is an explicit: rl.close()
-    rl.on('close', () => { process.exit(0); }); // Do fancier stuff here ?
-
-  }
+      // if (rp) process.stdout.write(JSON.stringify(response) + '\n');
+    }
 }
