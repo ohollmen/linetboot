efc96e85cdb6e1d75ec8133e96679dfc3a02d081
Olli Hollmen <olli.hollmen@gmail.com>
2026-05-11 09:04:16 -0700

    Fine-tune implementation, add comments.

--- afa.js
+++ afa.js
@@ -17,27 +17,38 @@
       - See also: https://cloud.google.com/artifact-registry/docs/reference/rest
         (https://cloud.google.com/artifact-registry/docs/reference/rest#rest-resource:-v1.projects.locations.repositories.dockerimages)
  * TODO: Expand to cover more apis ?
- * - Note: It seems lot of these calls are memrely universal Docker Registry API calls
+ * - Note: It seems lot of these calls are merely universal Docker Registry API calls
  * - Registry API doc: https://docker-docs.uclv.cu/registry/spec/api/
+ * - Make imgpath: "..." => imgpaths: [], pass ?imgpath=...
+ * ## JSON cache file
+ * - AFA: AoO - Objects are images found under a "folder" cfg.imgrepo
+ *   - Gotten w. "_catalog" and "/tags/list" (See lsimgs)
+ *   - Each image has one or more tags (e.g. latest, 1.0.15, 24.04)
+ *   - Each tagged version has manifest: `${imgpath}/manifests/${tag}`, where tag is also called "reference" (like git)
 */
 var axios = require("axios");
 var Mustache = require("mustache");
-var fs = require("fs");
+var fs    = require("fs");
 var async = require("async");
 // API URL rules: https://docker-docs.uclv.cu/registry/spec/api/
-// - Shows API starting w. /v2/, <name> is the *image* name
-// - Art seems to namespace their container registry with their own prefix
-//   - imgrepo (below) is a single (non-path hier.) token ([\w\-]+, e.g. a-b-c)
+// - Shows API starting w. /v2/, <name> is the *image* path/name aka reponame
+// - Art seems to namespace their container registry with their own prefix (artifactory/)
+//   - imgrepo (below) is a single (non-path hier.) token ([\w\-]+, e.g. a-b-c) specific to artif.
+//   - name afarepo
+//   - Also it seems URL-escaping path following v2/ is artifactory specific.
 var apiprefix = "api/docker/{{ imgrepo }}/v2/";
+// For "neutral" registry, the prefix might be:
+//var apiprefix = "v2/"; // Followed by (spec:) "reponame" aka (here) imgpath
 var ahdr = "X-JFrog-Art-Api"; // Auth header (See Introduction ..., also Bearer token, basic w. user:token)
 var cfg;
 var creds = {};
 var ropts = {headers: {}};
+// Init !
 function init(_mcfg) {
-    if (!_mcfg) { throw "No main config"; }
+    if (!_mcfg) { throw "No main config passed"; }
     if (!_mcfg["afa"]) { console.error("No afa config (return)"); return; } // throw "No afa config within main config";
     cfg = _mcfg["afa"];
-    if (!cfg.host) { console.log( "No  config host" ); return; }
+    if (!cfg.host) { console.log( "No config host" ); return; }
     //if (!cfg.user) { console.log( "No  config user" ); return; }
     //if (!cfg.pass) { console.log( "No  config pass" ); return; }
     if (!cfg.token) { console.log( "No  config token" ); return; }
@@ -47,47 +58,59 @@ function init(_mcfg) {
     //creds = cfg.user+":"+cfg.pass;
     creds[ahdr] = cfg.token;
     ropts.headers[ahdr] = cfg.token; // axios
+    if (cfg.apiprefix) { apiprefix = cfg.apiprefix; }
+    if (cfg.afarepo) {  } // Prep  by templating: apiprefix = Mustache.render(apiprefix, cfg)
     console.log(cfg);
 }
+// Make various API URL paths derived from the fixed / constant prefix part of the path
+// Typical appended "path" parts: _catalog
 function mkurl(path) {
-  // Use imgrepo from cfg.
-  var prepath = Mustache.render(apiprefix, cfg);
-  return "https://"+cfg.host+"/"+prepath+""+path;
+  // Use "imgrepo" (TODO: afarepo) from cfg.
+  // if (cfg.afarepo) {
+  var prefixpath = Mustache.render(apiprefix, cfg);
+  return `https://${cfg.host}/${prefixpath}${path}`;
 }
 function curl(url) {
   console.log(`curl -H '${ahdr}:${cfg.token}' '${url}'`);
 }
-// List images 
+// List images and optionally tags (CLI).
+// For now this is only CLI op and web frontend uses cached version produced here.
+// Saves docker_images_tags.json (AoO)
 function lsimgs(p) {
   var url = mkurl("_catalog");
   curl(url);
   var re = null;
   if (cfg.excpatt) { re = new RegExp(cfg.excpatt, 'g'); }
-  var imgidx = {};
+  var imgidx = {}; // Map: imgpath: 1
   axios.get(url, ropts).then( (resp) => {
+    // { "repositories": [ <name>, ... ] } these are paths
     var d = resp.data;
-    if (!d.repositories) { throw "No image paths listed !!!"; }
+    if (!d.repositories) { throw "No image paths listed (for _catalog query) !"; }
     if (re) { d.repositories = d.repositories.filter( (p) => { return ! p.match(re); }); }
-    console.log(JSON.stringify(d, null, 2));
-    console.log(d.repositories.length + " Items");
-    if (!p.op == 'imgstags') { return; }
-    // op: 'imgstags' - Optionally continue by fetching tags for each
+    console.log(d.repositories.length + "  Repositories by _catalog");
+    console.log(`REPOS:`+JSON.stringify(d, null, 2));
+    
+    if (p.op != 'imgstags') { return; } // TODO: !p.op == 'imgstags' Incorrect ? Changed
+    // op: 'imgstags' - Optionally continue by fetching tags for each "repo"
     async.mapSeries(d.repositories, image_tags, function (err, ress) {
       if (err) { var xerr = "Error iterating image paths:"+err; console.log(xerr); return;  }
-      var fn = "/tmp/docker_images_tags.json";
+      // Detect web: if (!p.op && p.cb) { return p.cb(ress); }
+      var fn = "/tmp/docker_images_tags.json"; // cfg.storpath || 
+      // if (fs.existsSync(fn)) { console.error(`output file ${fn} exists.`); return; }
       fs.writeFileSync( fn,  JSON.stringify(ress, null, 2) , {encoding: "utf8"} );
       //console.log(imgidx);
       console.log("Wrote to "+fn);
     });
-  }).catch( (ex) => { console.log("Error requesting: "+ex); });
+  }).catch( (ex) => { console.log(`Error requesting: ${ex}`); });
   // List tags for image by it's full path (w/o tag)
   // TODO: Place outside, but note: imgidx (imgidx[imgpath])
   function image_tags(imgpath, cb) {
     //if (!imgpath) { return cb("No imagepath", null); }
-    console.error("Querying tags for img: "+imgpath);
+    console.error(`Querying tags for img: ${imgpath}`);
     imgidx[imgpath] = 1; // TEST ()
-    var e = {imgpath: imgpath, tags: []}; // TODO
-    var url = mkurl(taglist_urlpath(imgpath));
+    var e = { imgpath: imgpath, tags: [] }; // Pass to completion cb(null, e)
+    var url = mkurl(taglist_urlpath(imgpath)); //  "/tags/list"
+    curl(url);
     axios.get(url, ropts).then( (resp) => {
       var d = resp.data;
       //if (d.tags != imgpath) { console.error("Warning: mismatch in imgpath"); } // assert
@@ -97,50 +120,54 @@ function lsimgs(p) {
       e.tags = d.tags;
       //return cb(null, imgpath); // Simple
       return cb(null, e);
-    }).catch( (ex) => { console.log("Error requesting: "+ex); });
+    }).catch( (ex) => { console.log(`Error requesting: ${ex}`); });
     
   }
 }
-// Create a taglist URL with imgpath '/' escaped to '%2F'
-// TODO: ensure 
+// Create a taglist URL-path with imgpath '/' escaped to '%2F' and optional appended part added.
+// Default append: "/tags/list", Others: /manifests/${tag}
+// TODO: rename imgpath_urlpath
 function taglist_urlpath(imgpath, append) {
-  imgpath = imgpath.replace(/\//g, '%2F');
+  imgpath = imgpath.replace(/\//g, '%2F'); // Only on afa
   append = append || "/tags/list";
-  var urlappend = imgpath + append; // "/tags/list";
+  var urlappend = imgpath + append;
   return urlappend;
 }
 function lstags(p) {
   // Even tail must be parametrized here. The first path part must have '/' url-escaped to '%2F'.
   var imgpath = p.imgpath || cfg.imgpath; // fall back to example image path from config
   if (!imgpath) { usage("No docker image path present for tags listing.") }
-  var url = mkurl(taglist_urlpath(imgpath));
-  //curl(url);
+  var url = mkurl(taglist_urlpath(imgpath)); // "/tags/list"
+  curl(url);
   axios.get(url, ropts).then( (resp) => {
     var d = resp.data;
     console.log(JSON.stringify(d, null, 2));
   
   }).catch( (ex) => { console.log("Error requesting: "+ex); });
 }
-/** List repo image manifest
- * Note: Lots of valuable info comes in HTTP Headers (As hinted on https://distribution.github.io/distribution/spec/api/, Digest Header) !!!
+/** List repo image manifest (for CLI or Web)
+ * Note: Lots of valuable info comes in HTTP Headers - as hinted on:
+ * https://distribution.github.io/distribution/spec/api/, Digest Header !!!
  */
 function lsmani(p) {
   var imgpath = p.imgpath || cfg.imgpath;
   var tag = p.tag || "latest";
-  console.log(`lsmani: imgpath=${imgpath}&tag=${tag}`);
-  var url = mkurl(taglist_urlpath(imgpath, "/manifests/"+tag)); // latest
+  console.log(`lsmani(p): imgpath=${imgpath}&tag=${tag} (imgpath un-encoded)`);
+  var url = mkurl(taglist_urlpath(imgpath, `/manifests/${tag}`)); // e.g. latest
   curl(url);
   axios.get(url, ropts).then( (resp) => {
     var d = resp.data;
     console.log(resp.headers); // docker-content-digest, x-artifactory-id
     d.cont_digest = resp.headers["docker-content-digest"]; // 64B hex
-    d.art_id = resp.headers["x-artifactory-id"]; // Only 40B (hex)
-    if (d.fsLayers && (d.fsLayers.length == 1)) { d.note = "Images with single layer are often vendor/open source project base images."; }
-    var h = history_list(d);
-    if (h) { console.log(JSON.stringify(h, null, 2)); }
-    if (p.cb) { p.cb(null, d); }
-    else { console.log(JSON.stringify(d, null, 2)); }
-  }).catch( (ex) => { console.log("Error requesting: "+ex); });
+    d.art_id      = resp.headers["x-artifactory-id"]; // Only 40B (hex)
+    if (d.fsLayers && (d.fsLayers.length == 1)) {
+      d.note = "Images with single layer are often vendor/open source project base images.";
+    }
+    var h = history_list(d); // Layer history from retrieved data d
+    if (h) { console.log("HISTORY:"+ JSON.stringify(h, null, 2)); }
+    if (p.cb) { return p.cb(null, d); }
+    else { console.log(JSON.stringify(d, null, 2)); } // CLI
+  }).catch( (ex) => { console.log(`Error requesting: ${ex}`); });
 }
 /* Parse History in manifest (schema 1) "history" (AoO) member.
 * Each entry (obj) is expected to be in format: {v1Compatibility}
@@ -160,7 +187,7 @@ function history_list(m) {
   if (!Array.isArray(h)) { console.log("Manifest history is not in array !!"); return null; }
   // consider: throwaway (true/false), j.container_config.Cmd[0] (Seems always an array of 1 item )
   // TODO: try {} catch (ex) {}
-  return h.map( (hi) => {
+  return h.map( (hi) => { // History item
     var j = JSON.parse(hi.v1Compatibility);
     console.log(j);
     // Consult different member for actual simple command
@@ -171,13 +198,14 @@ function history_list(m) {
   });
 }
 var acts = [
+  // {"id": "apiinfo",    "title": "Api Info (v2/)","cb": apiinfo, }, // function apiinfo(p) {} 200 OK/ 401 / 404
   // List / GET
   {"id": "imgs",    "title": "List Images (w/o tag)","cb": lsimgs, },
-  {"id": "imgstags","title": "List Images AND Tags", "cb": lsimgs, },
+  {"id": "imgstags","title": "List Images AND Tags (writes docker_images_tags.json)", "cb": lsimgs, },
   {"id": "tags",    "title": "List Tags",       "cb": lstags, },
   {"id": "mani",    "title": "List Manifest",   "cb": lsmani, },
   // {"id": "urls",   "title": "List Test URLs (curl)",   "cb": ????, },
-  {"id": "conf",   "title": "Show default imgrepo,imgpath",
+  {"id": "conf",   "title": "Show default imgrepo,imgpath,storpath",
      "cb": (p) => {
      if (!cfg) { console.error("No afa config !"); return; }
      console.log(`imgrepo: ${cfg.imgrepo}\nimgpath: ${cfg.imgpath}\nstorpath: ${cfg.storpath}`);
@@ -189,6 +217,7 @@ var clopts = [
   ["r", "imgrepo=ARG", "Image repo label"],
   ["p", "imgpath=ARG", "Image path to list"],
   ["t", "tag=ARG", "Tag for the image (given by --imgpath)"],
+  //["s", "save=ARG", "Filename for file to save (for op imgtags)"],
 ];
 
 function usage(msg) {
@@ -199,18 +228,27 @@ function usage(msg) {
   clopts.forEach( (o) => { console.log("- "+o[1] + " - " + o[2]); });
   process.exit(1);
 }
+////////////////// Web handlers /////////////////////////
+// List images (from top level item) by using cached data (/afaimgs).
+// TODO: Allow passing top-level repo to choose file for *that* toprepo or filter by that repo ?
 function afaimgs(req, res) {
+  let jr = {status: "err", "msg": "Could not serve images info. "};
   var fn = cfg.storpath;
-  if (!fs.existsSync(fn)) { fn = process.env.HOME +"/.linetboot/afa/docker_images_tags.json"; }
-  var imgs = require(fn);
+  if (!fn) { fn = process.env.HOME +"/.linetboot/afa/docker_images_tags.json"; }
+  if (!fs.existsSync(fn)) { jr.msg += "No cache file found !"; return res.json(jr); }
+  var imgs = require(fn); // Load cached !
+  if (!imgs) { jr.msg += "No valid (cached) JSON loaded !"; return res.json(jr); }
   res.json({status: "ok", data: imgs});
 }
+// Load image manifest by (full) imagepath and tag (from query params to /imgmani).
+// imgpath should be urlencoded by client ('/' => '%2F') E.g.: ?imgpath=path1%2Fpath2&tag=0.0.1
 function imgmani(req, res) {
   var jr = {"status": "err", "msg": "Could not produce Manifest info details ! "};
-  var fn = cfg.storpath;
+  // var fn = cfg.storpath; // NOT used here
   var q = req.query;
-  if (!q.imgpath) { jr.msg += "No imgpath passed"; return res.json(jr); }
-  if (!q.tag)     { jr.msg += "No tag passed"; return res.json(jr); }
+  if (!q.imgpath) { jr.msg += "No 'imgpath' passed in query"; return res.json(jr); }
+  if (!q.tag)     { jr.msg += "No 'tag' passed in query"; return res.json(jr); }
+  // Simulate CLI opts (p) for web mode
   var p = { tag: q.tag, imgpath: q.imgpath, cb: respond }; // q.imgpath / cfg.imgpath
   //p.tag = "0.0.13"; // TEST
   lsmani(p);
@@ -218,9 +256,6 @@ function imgmani(req, res) {
     if (err) { return res.json(jr); }
     return res.json({"status": "ok", "data": d});
   }
-  //if (!fs.existsSync(fn)) { fn = process.env.HOME +"/.linetboot/afa/docker_images_tags.json"; }
-  //var imgs = require(fn);
-  //res.json({status: "ok", data: imgs});
 }
 module.exports = {init: init, afaimgs: afaimgs, imgmani: imgmani};
 
