93a5945572c270ea5095d132b38de45ea39e1930
Olli Hollmen <olli.hollmen@gmail.com>
2026-06-06 20:17:27 -0700

    Set logo size explicitly.

--- web/js/hostviewdef.js
+++ web/js/hostviewdef.js
@@ -1545,7 +1545,7 @@ function riskadj_cell(val, item) {
    function grepo_remote_cell(val, item) {
     // Lookup icon from data of /config
     let grepo = datasets.cfg && datasets.cfg.grepo ? datasets.cfg.grepo : null;
-    let iconsym =  (grepo && grepo.remiconpath && grepo.remiconpath[val]) ? `<img src="${grepo.remiconpath[val]}">` : '';
+    let iconsym =  (grepo && grepo.remiconpath && grepo.remiconpath[val]) ? `<img src="${grepo.remiconpath[val]}" width="16px" height="16px">` : '';
      if (val && !item.remote_def) { return `${val} ${iconsym}`; }
      //if (!iconsym) {}
      return `<span style="color: #888888;">${item.remote_def} ${iconsym}</span>`;
